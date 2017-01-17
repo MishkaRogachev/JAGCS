5416ae6d97f70364c5c758ca8ee7c070d8d8b049
MishkaRogachev <mishkarogachev@gmail.com>
2017-01-17 14:48:01 +0300

    Idle English translations added

---
+++ translations/jagcs_en.ts
@@ -0,0 +1,408 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US" sourcelanguage="en_US">
+<context>
+    <name>ConnectionSettingsView</name>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/ConnectionSettingsView.qml" line="31"/>
+        <source>System ID</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/ConnectionSettingsView.qml" line="45"/>
+        <source>Component ID</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/ConnectionSettingsView.qml" line="75"/>
+        <source>No links present</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/ConnectionSettingsView.qml" line="103"/>
+        <source>Add Link</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/ConnectionSettingsView.qml" line="114"/>
+        <source>Serial</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/ConnectionSettingsView.qml" line="120"/>
+        <source>Udp</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>GuiSettingsView</name>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/GuiSettingsView.qml" line="23"/>
+        <source>UI size</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/GuiSettingsView.qml" line="47"/>
+        <source>Palette</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/GuiSettingsView.qml" line="66"/>
+        <source>Indoor</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/GuiSettingsView.qml" line="69"/>
+        <source>Outdoor</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/GuiSettingsView.qml" line="75"/>
+        <source>Language</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>LinkView</name>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="17"/>
+        <source>None</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="27"/>
+        <source>Serial device</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="43"/>
+        <source>Baud rate</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="60"/>
+        <source>Port</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="76"/>
+        <source>Auto response</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="86"/>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="93"/>
+        <source> b/s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="100"/>
+        <source>Drops: </source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="111"/>
+        <source>Disconnect</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="112"/>
+        <source>Connect</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/Connection/LinkView.qml" line="121"/>
+        <source>Remove</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MissionItemView</name>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="13"/>
+        <source>UNKNOWN</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="13"/>
+        <source>HOME</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="13"/>
+        <source>TAKEOFF</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="14"/>
+        <source>WAYPOINT</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="14"/>
+        <source>LOITER</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="14"/>
+        <source>CONTINUE</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="15"/>
+        <source>RETURN</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="15"/>
+        <source>LANDING</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="29"/>
+        <source>#</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="73"/>
+        <source>Lat.:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="88"/>
+        <source>Lon.:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="104"/>
+        <source>Alt.:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="120"/>
+        <source>Rel.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="130"/>
+        <source>Pitch:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="146"/>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="162"/>
+        <source>Radius:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionItemView.qml" line="178"/>
+        <source>Yaw:</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>MissionView</name>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Mission/MissionView.qml" line="123"/>
+        <source>Add Item</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>NetworkSettingsView</name>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="34"/>
+        <source>Proxy type</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="45"/>
+        <source>Host name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="51"/>
+        <source>Enter hostname</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="56"/>
+        <source>Port</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="68"/>
+        <source>User name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="74"/>
+        <source>Enter user name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="79"/>
+        <source>User password</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="85"/>
+        <source>Enter password</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="96"/>
+        <source>Restore</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/NetworkSettingsView.qml" line="102"/>
+        <source>Apply</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsView</name>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/SettingsView.qml" line="21"/>
+        <source>GUI</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/SettingsView.qml" line="21"/>
+        <source>Connection</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/SettingsView.qml" line="22"/>
+        <source>Video</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/SettingsView.qml" line="22"/>
+        <source>Networking</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/SettingsView.qml" line="30"/>
+        <source>Make defaults</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>VehicleView</name>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Flight/VehicleView.qml" line="43"/>
+        <source>V:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Flight/VehicleView.qml" line="45"/>
+        <source>v</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Flight/VehicleView.qml" line="49"/>
+        <source>C:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Flight/VehicleView.qml" line="51"/>
+        <source>A</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Flight/VehicleView.qml" line="57"/>
+        <source>DISARM</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Flight/VehicleView.qml" line="57"/>
+        <source>ARM</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>VideoSettingsView</name>
+    <message>
+        <location filename="../sources/presentation/qml/Views/Settings/VideoSettingsView.qml" line="27"/>
+        <source>Video source</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>presentation::ConnectionSettingsPresenter</name>
+    <message>
+        <location filename="../sources/presentation/presenters/settings_presenter/connection_settings_presenter/connection_settings_presenter.cpp" line="125"/>
+        <source>UDP</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/presenters/settings_presenter/connection_settings_presenter/connection_settings_presenter.cpp" line="137"/>
+        <source>Serial</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>presentation::FlightPresenter</name>
+    <message>
+        <location filename="../sources/presentation/presenters/flight_presenter/flight_presenter.cpp" line="71"/>
+        <source>MAV %1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>presentation::MissionPresenter</name>
+    <message>
+        <location filename="../sources/presentation/presenters/mission_presenter/mission_presenter.cpp" line="74"/>
+        <source>Mission</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/presenters/mission_presenter/mission_presenter.cpp" line="131"/>
+        <source>MAV %1</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>presentation::NetworkSettingsPresenter</name>
+    <message>
+        <location filename="../sources/presentation/presenters/settings_presenter/network_settings_presenter/network_settings_presenter.cpp" line="25"/>
+        <source>No Proxy</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/presenters/settings_presenter/network_settings_presenter/network_settings_presenter.cpp" line="26"/>
+        <source>Default</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/presenters/settings_presenter/network_settings_presenter/network_settings_presenter.cpp" line="27"/>
+        <source>SOCKS 5</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/presenters/settings_presenter/network_settings_presenter/network_settings_presenter.cpp" line="28"/>
+        <source>HTTP</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/presenters/settings_presenter/network_settings_presenter/network_settings_presenter.cpp" line="29"/>
+        <source>Caching HTTP</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../sources/presentation/presenters/settings_presenter/network_settings_presenter/network_settings_presenter.cpp" line="30"/>
+        <source>Caching FTP</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>presentation::VideoSettingsPresenter</name>
+    <message>
+        <location filename="../sources/presentation/presenters/settings_presenter/video_settings_presenter/video_settings_presenter.cpp" line="48"/>
+        <source>none</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
