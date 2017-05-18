eb456b8aca11ba47f7f055a101b6b50041e3845c
MishkaRogachev <mishkarogachev@gmail.com>
2017-05-18 21:25:36 +0300

    DefaultParamsMigration added

--- sources/db/migrator/factory/db_migration_factory.cpp
+++ sources/db/migrator/factory/db_migration_factory.cpp
@@ -2,6 +2,7 @@
 
 // Internal
 #include "init_migration.h"
+#include "default_params_migration.h"
 
 using namespace db;
 
@@ -10,6 +11,7 @@ MigrationList DbMigrationFactory::create()
     MigrationList list;
 
     list.append(new InitMigration());
+    list.append(new DefaultParamsMigration());
 
     return list;
 }
---
+++ sources/db/migrator/migrations/default_params_migration.cpp
@@ -0,0 +1,60 @@
+#include "default_params_migration.h"
+
+// Qt
+#include <QCoreApplication>
+#include <QDebug>
+
+// Internal
+#include "settings_provider.h"
+
+#include "db_facade.h"
+#include "link_description.h"
+#include "vehicle_description.h"
+#include "mission.h"
+#include "mission_assignment.h"
+
+using namespace db;
+
+bool DefaultParamsMigration::up()
+{
+    DbFacade facade;
+
+    LinkDescriptionPtr defaultUdpLink = LinkDescriptionPtr::create();
+    defaultUdpLink->setType(LinkDescription::Udp);
+    defaultUdpLink->setPort(domain::SettingsProvider::value(settings::communication::port).toInt());
+    defaultUdpLink->setName(qApp->translate("DefaultParamsMigration", "Default UDP Link"));
+    defaultUdpLink->setAutoConnect(true);
+    facade.save(defaultUdpLink);
+
+    LinkDescriptionPtr defaultSerialLink = LinkDescriptionPtr::create();
+    defaultSerialLink->setType(LinkDescription::Serial);
+    defaultSerialLink->setBaudRate(domain::SettingsProvider::value(settings::communication::baudRate).toInt());
+    defaultSerialLink->setName(qApp->translate("DefaultParamsMigration", "Default Serial Link"));
+    defaultSerialLink->setAutoConnect(true);
+    facade.save(defaultSerialLink);
+
+    VehicleDescriptionPtr defaultVehicle = VehicleDescriptionPtr::create();
+    defaultVehicle->setMavId(1);
+    defaultVehicle->setName(qApp->translate("DefaultParamsMigration", "Default Vehicle"));
+    facade.save(defaultVehicle);
+
+    MissionPtr defaultMission = MissionPtr::create();
+    defaultMission->setName(qApp->translate("DefaultParamsMigration", "Default Mission"));
+    facade.save(defaultMission);
+
+    facade.assign(defaultMission->id(), defaultVehicle->id());
+
+    return true;
+}
+
+bool DefaultParamsMigration::down()
+{
+    // Delete default values
+
+    return true;
+}
+
+QDateTime DefaultParamsMigration::version() const
+{
+    return QDateTime::fromString("14:14:14-18.05.2017", format);
+}
