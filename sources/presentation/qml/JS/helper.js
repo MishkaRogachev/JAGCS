318ee2e2d22244d9ba2e218d8ed00b36cceca968
MishkaRogachev <mishkarogachev@gmail.com>
2017-06-06 14:39:45 +0300

    DMS calculation fixed

--- sources/presentation/qml/JS/helper.js
+++ sources/presentation/qml/JS/helper.js
@@ -14,17 +14,23 @@ function pad(num, size) {
     return str;
 }
 
-function degreesToDms(degrees) {
-    var deg = degrees | 0;
-    var frac = Math.abs(degrees - deg);
-    var min = (frac * 60) | 0;
-    var sec = frac * 3600 - min * 60;
+function degreesToDms(degrees, lng) {
+    var deg = Math.floor(degrees);
+    var frac = (degrees - deg) * 60;
+    var min = Math.floor(frac);
+    var sec = Math.round((frac - min) * 6000) / 100;
+
+    if (sec >= 60) { sec -= 60; min++; }
+    if (min >= 60) { min -= 60; deg++; }
+    deg = Math.abs(deg)
+    var degLimit = (lng ? 180 : 90);
+    if (deg >= degLimit) deg -= degLimit;
 
     return {
         sign: degrees < 0 ? -1 : 1,
-        deg:  Math.abs(deg),
+        deg: deg,
         min: min,
-        sec: sec
+        sec: sec.toFixed(2)
     }
 }
 
@@ -33,22 +39,23 @@ function dmsToDegree(deg, min, sec) {
 }
 
 function degreesToDmsString(degrees, lng) {
-    var dms = isNaN(degrees) ? degreesToDms(0) : degreesToDms(degrees);
+    var dms = isNaN(degrees) ? degreesToDms(0, lng) : degreesToDms(degrees, lng);
     return pad(dms.deg, 3) + "\u00B0" +
            pad(dms.min, 2) + "\'" +
-           pad(dms.sec.toFixed(2), 5) + "\"" +
+           pad(dms.sec, 5) + "\"" +
            (dms.sign < 0 ? lng ? "W" : "S" : lng ? "E" : "N");
 }
 
 function dmsStringToDegree(string, lng) {
     var split = string.split("\u00B0");
-    if (split.cout < 2) return 0.0;
+    if (split.length < 2) return NaN;
     var deg = split[0];
     split = split[1].split("\'");
-    if (split.cout < 2) return 0.0;
+    if (split.length < 2) return NaN;
     var min = split[0];
     split = split[1].split("\"");
     var sec = split[0];
     var sign = split[1] === "W" || split[1] === "S" ? -1 : 1;
     return dmsToDegree(sign * parseInt(deg), parseInt(min), parseFloat(sec));
 }
+
