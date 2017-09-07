700b407312c2dccc60791a127b196963acad2ca8
MishkaRogachev <mishkarogachev@gmail.com>
2017-09-07 19:32:58 +0300

    Palette c++ type added

---
+++ sources/presentation/common/palette/palette.cpp
@@ -0,0 +1,213 @@
+#include "palette.h"
+
+using namespace presentation;
+
+int Palette::controlBaseSize() const
+{
+    return m_controlBaseSize;
+}
+
+void Palette::setControlBaseSize(int controlBaseSize)
+{
+    m_controlBaseSize = controlBaseSize;
+}
+
+int Palette::fontPixelSize() const
+{
+    return m_fontPixelSize;
+}
+
+void Palette::setFontPixelSize(int fontPixelSize)
+{
+    m_fontPixelSize = fontPixelSize;
+}
+
+int Palette::spacing() const
+{
+    return m_spacing;
+}
+
+void Palette::setSpacing(int spacing)
+{
+    m_spacing = spacing;
+}
+
+int Palette::margins() const
+{
+    return m_margins;
+}
+
+void Palette::setMargins(int margins)
+{
+    m_margins = margins;
+}
+
+int Palette::padding() const
+{
+    return m_padding;
+}
+
+void Palette::setPadding(int padding)
+{
+    m_padding = padding;
+}
+
+QColor Palette::backgroundColor() const
+{
+    return m_backgroundColor;
+}
+
+void Palette::setBackgroundColor(QColor backgroundColor)
+{
+    m_backgroundColor = backgroundColor;
+}
+
+QColor Palette::sunkenColor() const
+{
+    return m_sunkenColor;
+}
+
+void Palette::setSunkenColor(QColor sunkenColor)
+{
+    m_sunkenColor = sunkenColor;
+}
+
+QColor Palette::raisedColor() const
+{
+    return m_raisedColor;
+}
+
+void Palette::setRaisedColor(QColor raisedColor)
+{
+    m_raisedColor = raisedColor;
+}
+
+QColor Palette::buttonColor() const
+{
+    return m_buttonColor;
+}
+
+void Palette::setButtonColor(QColor buttonColor)
+{
+    m_buttonColor = buttonColor;
+}
+
+QColor Palette::textColor() const
+{
+    return m_textColor;
+}
+
+void Palette::setTextColor(QColor textColor)
+{
+    m_textColor = textColor;
+}
+
+QColor Palette::selectedTextColor() const
+{
+    return m_selectedTextColor;
+}
+
+void Palette::setSelectedTextColor(QColor selectedTextColor)
+{
+    m_selectedTextColor = selectedTextColor;
+}
+
+QColor Palette::highlightColor() const
+{
+    return m_highlightColor;
+}
+
+void Palette::setHighlightColor(QColor highlightColor)
+{
+    m_highlightColor = highlightColor;
+}
+
+QColor Palette::selectionColor() const
+{
+    return m_selectionColor;
+}
+
+void Palette::setSelectionColor(QColor selectionColor)
+{
+    m_selectionColor = selectionColor;
+}
+
+QColor Palette::negativeColor() const
+{
+    return m_negativeColor;
+}
+
+void Palette::setNegativeColor(QColor negativeColor)
+{
+    m_negativeColor = negativeColor;
+}
+
+QColor Palette::neutralColor() const
+{
+    return m_neutralColor;
+}
+
+void Palette::setNeutralColor(QColor neutralColor)
+{
+    m_neutralColor = neutralColor;
+}
+
+QColor Palette::positiveColor() const
+{
+    return m_positiveColor;
+}
+
+void Palette::setPositiveColor(QColor positiveColor)
+{
+    m_positiveColor = positiveColor;
+}
+
+QColor Palette::trackColor() const
+{
+    return m_trackColor;
+}
+
+void Palette::setTrackColor(QColor trackColor)
+{
+    m_trackColor = trackColor;
+}
+
+QColor Palette::missionColor() const
+{
+    return m_missionColor;
+}
+
+void Palette::setMissionColor(QColor missionColor)
+{
+    m_missionColor = missionColor;
+}
+
+QColor Palette::activeMissionColor() const
+{
+    return m_activeMissionColor;
+}
+
+void Palette::setActiveMissionColor(QColor activeMissionColor)
+{
+    m_activeMissionColor = activeMissionColor;
+}
+
+QColor Palette::skyColor() const
+{
+    return m_skyColor;
+}
+
+void Palette::setSkyColor(QColor skyColor)
+{
+    m_skyColor = skyColor;
+}
+
+QColor Palette::groundColor() const
+{
+    return m_groundColor;
+}
+
+void Palette::setGroundColor(QColor groundColor)
+{
+    m_groundColor = groundColor;
+}
---
+++ sources/presentation/common/palette/palette.h
@@ -0,0 +1,132 @@
+#ifndef PALETTE_H
+#define PALETTE_H
+
+// Qt
+#include <QObject>
+#include <QColor>
+
+namespace presentation
+{
+    class Palette
+    {
+        Q_GADGET
+
+        Q_PROPERTY(int controlBaseSize READ controlBaseSize WRITE setControlBaseSize)
+        Q_PROPERTY(int fontPixelSize READ fontPixelSize WRITE setFontPixelSize)
+
+        Q_PROPERTY(int spacing READ spacing WRITE setSpacing)
+        Q_PROPERTY(int margins READ margins WRITE setMargins)
+        Q_PROPERTY(int padding READ padding WRITE setPadding)
+
+        Q_PROPERTY(QColor backgroundColor READ backgroundColor WRITE setBackgroundColor)
+        Q_PROPERTY(QColor sunkenColor READ sunkenColor WRITE setSunkenColor)
+        Q_PROPERTY(QColor raisedColor READ raisedColor WRITE setRaisedColor)
+        Q_PROPERTY(QColor buttonColor READ buttonColor WRITE setButtonColor)
+
+        Q_PROPERTY(QColor textColor READ textColor WRITE setTextColor)
+        Q_PROPERTY(QColor selectedTextColor READ selectedTextColor WRITE setSelectedTextColor)
+
+        Q_PROPERTY(QColor highlightColor READ highlightColor WRITE setHighlightColor)
+        Q_PROPERTY(QColor selectionColor READ selectionColor WRITE setSelectionColor)
+
+        Q_PROPERTY(QColor negativeColor READ negativeColor WRITE setNegativeColor)
+        Q_PROPERTY(QColor neutralColor READ neutralColor WRITE setNeutralColor)
+        Q_PROPERTY(QColor positiveColor READ positiveColor WRITE setPositiveColor)
+
+        Q_PROPERTY(QColor trackColor READ trackColor WRITE setTrackColor)
+        Q_PROPERTY(QColor missionColor READ missionColor WRITE setMissionColor)
+        Q_PROPERTY(QColor activeMissionColor READ activeMissionColor WRITE setActiveMissionColor)
+
+        Q_PROPERTY(QColor skyColor READ skyColor WRITE setSkyColor)
+        Q_PROPERTY(QColor groundColor READ groundColor WRITE setGroundColor)
+
+    public:
+        int controlBaseSize() const;
+        void setControlBaseSize(int controlBaseSize);
+
+        int fontPixelSize() const;
+        void setFontPixelSize(int fontPixelSize);
+
+        int spacing() const;
+        void setSpacing(int spacing);
+
+        int margins() const;
+        void setMargins(int margins);
+
+        int padding() const;
+        void setPadding(int padding);
+
+        QColor backgroundColor() const;
+        void setBackgroundColor(QColor backgroundColor);
+
+        QColor sunkenColor() const;
+        void setSunkenColor(QColor sunkenColor);
+
+        QColor raisedColor() const;
+        void setRaisedColor(QColor raisedColor);
+
+        QColor buttonColor() const;
+        void setButtonColor(QColor buttonColor);
+
+        QColor textColor() const;
+        void setTextColor(QColor textColor);
+
+        QColor selectedTextColor() const;
+        void setSelectedTextColor(QColor selectedTextColor);
+
+        QColor highlightColor() const;
+        void setHighlightColor(QColor highlightColor);
+
+        QColor selectionColor() const;
+        void setSelectionColor(QColor selectionColor);
+
+        QColor negativeColor() const;
+        void setNegativeColor(QColor negativeColor);
+
+        QColor neutralColor() const;
+        void setNeutralColor(QColor neutralColor);
+
+        QColor positiveColor() const;
+        void setPositiveColor(QColor positiveColor);
+
+        QColor trackColor() const;
+        void setTrackColor(QColor trackColor);
+
+        QColor missionColor() const;
+        void setMissionColor(QColor missionColor);
+
+        QColor activeMissionColor() const;
+        void setActiveMissionColor(QColor activeMissionColor);
+
+        QColor skyColor() const;
+        void setSkyColor(QColor skyColor);
+
+        QColor groundColor() const;
+        void setGroundColor(QColor groundColor);
+
+    private:
+        int m_controlBaseSize;
+        int m_fontPixelSize;
+        int m_spacing;
+        int m_margins;
+        int m_padding;
+        QColor m_backgroundColor;
+        QColor m_sunkenColor;
+        QColor m_raisedColor;
+        QColor m_buttonColor;
+        QColor m_textColor;
+        QColor m_selectedTextColor;
+        QColor m_highlightColor;
+        QColor m_selectionColor;
+        QColor m_negativeColor;
+        QColor m_neutralColor;
+        QColor m_positiveColor;
+        QColor m_trackColor;
+        QColor m_missionColor;
+        QColor m_activeMissionColor;
+        QColor m_skyColor;
+        QColor m_groundColor;
+    };
+}
+
+#endif // PALETTE_H
