54c0619b67f702f96baded9cb9e984e45fcf7ef9
MishkaRogachev <mishkarogachev@gmail.com>
2017-12-10 19:42:37 +0300

    QGamepad dependency temporary removed

--- sources/domain/joystick/joystick_controller.cpp
+++ sources/domain/joystick/joystick_controller.cpp
@@ -1,14 +1,14 @@
 #include "joystick_controller.h"
 
 // Qt
-#include <QGamepad>
+//#include <QGamepad>
 #include <QDebug>
 
 using namespace domain;
 
 JoystickController::JoystickController(int device, QObject* parent):
     QObject(parent)
-{
+{/*
     auto gamepads = QGamepadManager::instance()->connectedGamepads();
     if (device > -1 && device < gamepads.count())
     {
@@ -31,17 +31,17 @@ JoystickController::JoystickController(int device, QObject* parent):
     });
     connect(m_gamepad, &QGamepad::axisRightYChanged, this, [this](double value) {
             emit valueChanged(4, value);
-    });
+    });*/
 }
 
 float JoystickController::value(int axis) const
 {
     switch (axis)
     {
-    case 1: return m_gamepad->axisLeftX();
+    /*case 1: return m_gamepad->axisLeftX();
     case 2: return m_gamepad->axisLeftY();
     case 3: return m_gamepad->axisRightX();
-    case 4: return m_gamepad->axisRightY();
+    case 4: return m_gamepad->axisRightY();*/
     default: return 0;
     }
 }
