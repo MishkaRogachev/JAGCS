218579c6970ef21dc14aefefde9881f38633e208
MishkaRogachev <mishkarogachev@gmail.com>
2018-10-19 12:55:32 +0300

    Don't switch on MAVLink 2 forced

--- sources/communication/communicators/mavlink/mavlink_communicator.cpp
+++ sources/communication/communicators/mavlink/mavlink_communicator.cpp
@@ -181,14 +181,14 @@ void MavLinkCommunicator::onDataReceived(const QByteArray& data)
     {
         if (!mavlink_parse_char(channel, (quint8)data[pos], &message, &status)) continue;
 
-#ifdef MAVLINK_V2
-        // if we got MavLink v2, switch to on it!
-        mavlink_status_t* channelStatus = mavlink_get_channel_status(channel);
-        if (!(channelStatus->flags & MAVLINK_STATUS_FLAG_IN_MAVLINK1))
-        {
-            this->switchLinkProtocol(d->receivedLink, MavLink2);
-        }
-#endif
+//#ifdef MAVLINK_V2
+//        // if we got MavLink v2, switch to on it!
+//        mavlink_status_t* channelStatus = mavlink_get_channel_status(channel);
+//        if (!(channelStatus->flags & MAVLINK_STATUS_FLAG_IN_MAVLINK1))
+//        {
+//            this->switchLinkProtocol(d->receivedLink, MavLink2);
+//        }
+//#endif
 
         d->mavSystemLinks[message.sysid] = d->receivedLink;
 
